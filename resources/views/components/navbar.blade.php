d0cf1e70ffbc70f5521c3c8d1e46a5ece7c412c0
Deadpool345 <87096532+Deadpool345@users.noreply.github.com>
2024-03-07 23:47:50 -0600

    proyecto

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,25 @@
+
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <div class="container-fluid">
+        <div class="d-flex container-img">
+            <a href="#" class="navbar-brand">
+                <img src="{{ asset('img/logo.svg') }}" alt="logo" width="100" height="100">
+            </a>
+        </div>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarCollapse">
+            <div class="navbar-nav mx-auto">
+                <a href="{{ url('/') }}" class="nav-item nav-link text-secondary">Inicio</a>
+                <a href="#" class="nav-item nav-link text-secondary">Catalogo</a>
+                <a href="#" class="nav-item nav-link text-secondary">Sobre Nosotros</a>
+                <a href="#" class="nav-item nav-link text-secondary ">Contacto</a>
+            </div>
+            <div class="navbar-nav ">
+                <a href="#" class="nav-item boton" style="margin-rigth:2rem;">Registrarse</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <title>You Driver</title>
+</head>
+<body class="bg-primary">
+    @include('components.navbar')
+</body>
+</html>
